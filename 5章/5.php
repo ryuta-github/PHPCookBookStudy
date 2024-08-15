954b60b934e0c462b0b4457c94fa1325b737ed1e
ryuta-github <ryuta.gm@gmail.com>
2024-08-15 10:08:16 +0900

    浮動小数点

--- 5章/5.php
+++ 5章/5.php
@@ -4,12 +4,19 @@
  * 数値を検証する
  */
 
-$datas = [123, 'あいうえお', '999'];
+// $datas = [123, 'あいうえお', '999'];
 
-foreach ($datas as $data) {
-    if (is_numeric($data)) {
-        echo ($data);
-    } else {
-        echo '違うよ';
-    }
-}
+// foreach ($datas as $data) {
+//     if (is_numeric($data)) {
+//         echo ($data);
+//     } else {
+//         echo '違うよ';
+//     }
+// }
+
+/**
+ * 浮動小数点の計算
+ */
+
+// 0.17 ではなく、float(0.17000000000000004) となる
+var_dump(1 - 0.83);
