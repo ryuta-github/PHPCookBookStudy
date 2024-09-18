53f57f7c745e3340d1d119d5125ea7784dff5d4f
ryuta-github <ryuta.gm@gmail.com>
2024-09-18 09:08:29 +0900

    配列の切り取り,

--- 7章/7_6.php
+++ 7章/7_6.php
@@ -12,3 +12,23 @@ var_dump($array);
 // arrayの先頭に追加したい
 array_unshift($array, 'ZZZ');
 var_dump($array);
+
+
+// Your code here!
+
+$patient1 = ['name' => "takeda", 'age' => 34];
+$patient2 = ['name' => "hero", 'age' => 34];
+
+var_dump([...$patient1, ...$patient2]);
+var_dump(array_merge_recursive($patient1, $patient2));
+
+
+// $array_data = range('A','Z');
+// var_dump(array_slice($array_data,1,2));
+
+
+$array_data = range(1, 5);
+var_dump(array_slice($array_data, 4, 2, true));
+
+$array_data1 = ['a' => 'apple', 'b' => 'banana', 20 => 'cola', 'd' => 'caffee'];
+var_dump(array_slice($array_data1, 2, 3));
